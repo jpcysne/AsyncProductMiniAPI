838e94e6b5e61456cd319a68b5030b9fc2a68750
jpcysne <joaopaulorubio@hotmail.com>
2023-04-17 00:32:42 +0200

    Inicial Commit

---
+++ AsyncProductAPI/Dtos/ListingStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace AsyncProductAPI.Dtos
+{
+    public class ListingStatus
+    {
+        public string? RequestStatus { get; set; }
+        public string? EstimatedCompetionTime { get; set; }
+        public string? ResourceURL { get; set; }
+    }
+}
